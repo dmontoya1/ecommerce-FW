957095d4b20beb442fcf2f5663d144e98bca8379
Daniel Montoya <dmontoya.web@gmail.com>
2021-07-21 12:01:59 -0500

    Step 8

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,9 +1,44 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+def setup(browser):
+    if browser == "chrome":
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        print("Launching Chrome Browser..............")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(GeckoDriverManager().install())
+    elif browser == 'safari':
+        driver = webdriver.Safari()
+        print("Launching Safari Browser..............")
+    else:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
     return driver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+########### pytest HTML Report ################
+
+# It is hook for Adding Environment info to HTML Report
+def pytest_configure(config):
+    config._metadata['Project Name'] = 'nop Commerce'
+    config._metadata['Module Name'] = 'Customers'
+    config._metadata['Tester'] = 'Daniel Montoya'
+
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.mark.optionalhook
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
--- testCases/text_login.py
+++ testCases/text_login.py
@@ -1,5 +1,6 @@
 from pageObjects.LoginPage import LoginPage
 from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
 
 
 class Test001Login:
@@ -7,20 +8,28 @@ class Test001Login:
     username = ReadConfig.get_application_username()
     password = ReadConfig.get_application_password()
 
+    logger = LogGen.loggen()
+
     def test_home_page_title(self, setup):
+        self.logger.info("******************** Test001Login **********************")
+        self.logger.info("******************** Verifying Home Page Title **********************")
         self.driver = setup
         self.driver.get(self.baseURL)
         actual_title = self.driver.title
 
         if actual_title == "Your store. Login":
-            assert True
             self.driver.close()
+            self.logger.info("********************  Home Page Title test is passed **********************")
+            assert True
         else:
             self.driver.save_screenshot("Screenshots/test_home_page_title.png")
             self.driver.close()
+            self.logger.info("********************  Home Page Title test is Failed **********************")
             assert False
 
     def test_login(self, setup):
+        self.logger.info("******************** Test001Login **********************")
+        self.logger.info("******************** Testing Login **********************")
         self.driver = setup
         self.driver.get(self.baseURL)
         self.lp = LoginPage(self.driver)
@@ -30,9 +39,11 @@ class Test001Login:
         actual_title = self.driver.title
 
         if actual_title == "Dashboard / nopCommerce administration":
-            assert True
             self.driver.close()
+            self.logger.info("********************  Login test is passed **********************")
+            assert True
         else:
             self.driver.save_screenshot("Screenshots/test_login.png")
             self.driver.close()
+            self.logger.info("********************  Login test is failed **********************")
             assert False
